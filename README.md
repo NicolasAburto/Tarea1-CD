3979bce8350426c14c06f5f29296012feb96306e
Nicolás Aburto <naburtoa@ing.ucsc.cl>
2022-04-21 03:30:30 -0400

    Commit final

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ Manejo de pantalla en modalidad texto con movimientos aleatorios.
 Programa desarrollado en Python, junto al uso de módulos e interfaz gráfica.
 
 ## Instalación
-Descomprimir la aplicación y ejecutar "menu-opt.py"
+Descomprimir la aplicación y ejecutar "main.py"
 
 ## Importante
 Al momento de ejecutar la aplicación se debe ingresar en la interfaz el número del caballo apostante.
-
-
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #   Descipción:
 #    - Se creará un programa que simule una carrera de caballos con avance aleatorio.
 #    - Código desarrollado mediante metodología de programación modular, con el objetivo de reutilizar bibliotecas de funciones y/o métodos.
-#    -
+#    - Versión: 0.1 (Prueba y demostración)
 #
 #   Autores:
 #       - José Avilán (https://github.com/JoseAvilan)
@@ -14,7 +14,6 @@
 #       - Abril 2022. Apache 2.0.
 #
 #################################################################################################################
-
 import random
 import tkinter as tk
 import tkinter as tk
@@ -34,71 +33,84 @@ from threading import Thread
 from urllib.request import urlretrieve, urlcleanup
 import time
 
+def stop():
+    #REINICIA LAS BARRAS DE PROGRESO PARA COMENZAR OTRA CARRERA
+    my_progress.stop()
+    my_progress2.stop()
+    my_progress3.stop()
+    my_progress4.stop()
+
 def callback(*args):
     #DEVUELVE LA VARIABLE SELECCIONADA EN EL MENU DESPLEGABLE "CABALLOS"
     labelTest.configure(text="Va a apostar al {}".format(variable.get()))
 
 def carrera():
     #SE SIMULA LA CARRERA CON LOS 4 CABALLOS Y MUESTRA EN UNA NUEVA VENTANA SI ES QUE EL CABALLO ELEGIDO PERDIÓ O GANÓ
-    
-
     opcion = variable.get()
     caballo1 = 0
     caballo2 = 0
     caballo3 = 0
     caballo4 = 0
-    
-    while caballo1 < 100 and caballo2 < 100 and caballo3 < 100 and caballo4 < 100:
+
+    while caballo1 < 30 and caballo2 < 30 and caballo3 < 30 and caballo4 < 30:
 
         caballo1 += random.randrange(1, 10)
-        caballo2 += random.randrange(1, 10)
+        caballo2 += random.randrange(1, 10)      
         caballo3 += random.randrange(1, 10)
         caballo4 += random.randrange(1, 10)
             
+        for x in range(1):
+            my_progress['value'] += caballo1
+            my_progress2['value'] += caballo2
+            my_progress3['value'] += caballo3
+            my_progress4['value'] += caballo4
+            root.update_idletasks()
+            time.sleep(0.5)
         
         print("Caballo 1: " + str(caballo1))
         print("Caballo 2: " + str(caballo2)) 
         print("Caballo 3: " + str(caballo3)) 
         print("Caballo 4: " + str(caballo4)) 
 
-    if opcion == 'Caballo 1' and caballo1 > 99:
+    if opcion == 'Caballo 1' and caballo1 > 30:
         pop_label = Label(pop, text="El caballo 1 Ganó")
         pop_label.pack(pady=10)
-    elif opcion == 'Caballo 1' and caballo1 < 99:
+    elif opcion == 'Caballo 1' and caballo1 < 30:
         pop_label = Label(pop, text="El caballo 1 perdió")
         pop_label.pack(pady=10)
 
-    if opcion == 'Caballo 2' and caballo2 > 99:
+    if opcion == 'Caballo 2' and caballo2 > 30:
         pop_label = Label(pop, text="El caballo 2 Ganó")
         pop_label.pack(pady=10)
-    elif opcion == 'Caballo 2' and caballo2 < 99:    
+    elif opcion == 'Caballo 2' and caballo2 < 30:    
         pop_label = Label(pop, text="El caballo 2 perdió")
         pop_label.pack(pady=10)
 
-    if opcion == 'Caballo 3' and caballo3 > 99:
+    if opcion == 'Caballo 3' and caballo3 > 30:
         pop_label = Label(pop, text="El caballo 3 Ganó")
         pop_label.pack(pady=10)
-    elif opcion == 'Caballo 3' and caballo3 < 99:    
+    elif opcion == 'Caballo 3' and caballo3 < 30:    
         pop_label = Label(pop, text="El caballo 3 perdió")
         pop_label.pack(pady=10)
         
-    if opcion == 'Caballo 4' and caballo4 > 99:
+    if opcion == 'Caballo 4' and caballo4 > 30:
         pop_label = Label(pop, text="El caballo 4 Ganó")
         pop_label.pack(pady=10)
-    elif opcion == 'Caballo 4' and caballo4 < 99:    
+    elif opcion == 'Caballo 4' and caballo4 < 30:    
         pop_label = Label(pop, text="El caballo 4 perdió") 
         pop_label.pack(pady=10)
 
         
-
 #label boton
 def click():
     #AQUÍ SE MUESTRA LA VENTANA DEL FINAL DE LA CARRERA JUNTO AL TEXTO DE QUE SI GANÓ O PERDIÓ
+    
     global pop
     pop = Toplevel(root) 
+    pop.iconbitmap('cab-vec.ico')
     pop.title("Final de la carrera")
     pop.geometry("350x200")
-
+    
     carrera()
 
     my_frame = Frame(pop)
@@ -111,6 +123,7 @@ OptionList = [
 "Caballo 4"
 ] 
 
+###############################################     INTERFAZ   ######################################################################################################
 root = tk.Tk()
 
 bit = root.iconbitmap('cab-vec.ico')
@@ -128,7 +141,6 @@ variable.set(OptionList[0])
 opt = tk.OptionMenu(root, variable, *OptionList)
 opt.config(width=90)
 opt.pack(side="top")
-
 
 labelTest = tk.Label(text="")
 labelTest.pack(side="top")
@@ -149,6 +161,25 @@ button.pack(fill=tk.X, padx=5, pady=5)
 
 my_label = Label(root, text="")
 my_label.pack(pady=20)
+#######################################################################################################################################################################
 
+###############################################     BARRA DE PROGRESO    ##############################################################################################
+my_progress = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
+my_progress.pack(pady=20)
 
+my_progress2 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
+my_progress2.pack(pady=20)
+
+my_progress3 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
+my_progress3.pack(pady=20)
+
+my_progress4 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
+my_progress4.pack(pady=20)
+
+my_button = Button(root, text="Reiniciar", command=stop)
+my_button.pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
+########################################################################################################################################################################
 root.mainloop()
