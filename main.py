6a0a88b2274783d3ecefda44bfaa3d4110c77efa
Nicolás Aburto <naburtoa@ing.ucsc.cl>
2022-04-24 21:21:23 -0400

    codigo final

--- main.py
+++ main.py
@@ -14,226 +14,216 @@
 #       - Abril 2022. Apache 2.0.
 #
 #################################################################################################################
-import random
-import tkinter as tk
-import tkinter as tk
-from secrets import choice
-from tkinter import ttk
-from tkinter.messagebox import showinfo
-from tkinter import *
-from tkinter import ttk
 
-from itertools import count
-from logging import root
-from operator import length_hint
-from struct import pack
-import tkinter as tk
-from tkinter import HORIZONTAL, Button, Label, Tk, ttk
-from threading import Thread
-from urllib.request import urlretrieve, urlcleanup
-import time
+import random, time
+
+class ArgumentoException( Exception):
+    # Excepción a ser lanzadas por error en los argumentos de llamada a una función o método.
+    pass
 
 class Caballo:
-    def __init__( self, nombre = None, maxPasosPorTurno = 10):
-        """
-        Constructor.
-        @param nombre Nombre del caballo.
-        @param maxPasosPorTurno Máximo de pasos que puede alcanzar en un turno.
-        """
+
+    def __init__( self, nombre = None):
+        #CONSTRUCTOR
+
         self.nombre = nombre
-        self.maxPasosPorTurno = 10
-        self.__carreras = []
-        self.__pasosCarreraActual = 0
+        self.Carrera = []
+        self.PasosCaballo = 0
+
+    def __str__( self):
+        # DEVUELVE EL OBJETO COMO UNA CADENA, COMO UN TOSTRING()
+        return self.nombre
+
+    @property
+    def nombre( self):
+        # DEVUELVE EL NOMBRE DEL CABALLO
+
+        return self.__nombre
+
+    @nombre.setter
+    def nombre( self, nombre = None):
+        # DEFINE EL NOMBRE A LOS CABALLOS
+
+        if nombre is None:
+            self.__nombre = "<Name>"
         
-    def MAX_PASOS_POR_TURNO():
-        """
-        Obtenertener el valor por defecto del número máximo de pasos por turno 
-        que puede dar un caballo.
-        @return Valor por defecto de número máximo de pasos por turno.
-        """
-        return Caballo.__MAX_PASOS_POR_TURNO
+        nombre = str( nombre).strip()
+        self.__nombre = "<Name>" if not nombre else nombre 
 
-def stop():
-    #REINICIA LAS BARRAS DE PROGRESO PARA COMENZAR OTRA CARRERA
-    my_progress.stop()
-    my_progress2.stop()
-    my_progress3.stop()
-    my_progress4.stop()
+    @property
+    def PasosCarrera( self):
+        # DEVUELVE EL NÚMERO DE PASOS EN LA CARRERA QUE LLEVA HASTA ESE MOMENTO
+        
+        return self.PasosCaballo
 
-def callback(*args):
-    #DEVUELVE LA VARIABLE SELECCIONADA EN EL MENU DESPLEGABLE "CABALLOS"
-    labelTest.configure(text="Va a apostar al {}".format(variable.get()))
+    def AñadirCarrera( self, carrera):
+        # AÑADIR UNA CARRERA PARA QUE EL CABALLO PUEDA PARTICIPAR
+        
+        self.Carrera.append( carrera)
+        self.PasosCaballo = 0
 
-def carrera():
-    #SE SIMULA LA CARRERA CON LOS 4 CABALLOS Y MUESTRA EN UNA NUEVA VENTANA SI ES QUE EL CABALLO ELEGIDO PERDIÓ O GANÓ
-    opcion = variable.get()
-    caballo1 = 0
-    caballo2 = 0
-    caballo3 = 0
-    caballo4 = 0
+    def Avanzar( self):
+        #AVANZA UN NÚMERO DE PASOS ALEATORIO ENTRE LOS VALORES 0 Y 9, EN SU TURNO CORRESPONDIENTE
 
-    while caballo1 < 30 and caballo2 < 30 and caballo3 < 30 and caballo4 < 30:
+        self.PasosCaballo += random.randrange( 0, 10 )
 
-        caballo1 += random.randrange(1, 10)
-        caballo2 += random.randrange(1, 10)      
-        caballo3 += random.randrange(1, 10)
-        caballo4 += random.randrange(1, 10)
+class Carrera:
+
+    __PASOS_META = 50
+
+    def __init__( self, nombre = None):
+        # CONSTRUCTOR
+       
+        self.PasosMeta = 50
+        self.nombre = nombre
+        self.__caballos = dict()
+        self.__CaballoGanador = []
+
+    def PASOS_META():
+        # DEVUELVE EL NÚMERO DE PASOS EN LA QUE ESTÁ LA META
+
+        return Carrera.__PASOS_META
+
+    @property
+    def nombre( self):
+        # DEVUELVE EL NOMBRE DEL CABALLO
+       
+        return self.__nombre
+
+    @nombre.setter
+    def nombre( self, nombre = None):
+        # DEFINE EL NOMBRE DE LA CARRERA
+     
+        if nombre is None:
+            self.__nombre = "<Sin Nombre>"
+        
+        nombre = str( nombre).strip()
+        self.__nombre = "<Sin nombre>" if not nombre else nombre 
+
+    @property
+    def PasosMeta( self):
+        # DEVUELVE EL NÚMERO DONDE SE ENCUENTRA LA META
+       
+        return self.__PasosMeta
+
+    @PasosMeta.setter
+    def PasosMeta( self, PasosMeta = None):
+        # DEFINE EL NUMERO DE PASOS EN LA QUE SE ENCUENTRA LA META
+
+        self.__PasosMeta = PasosMeta
+
+    @property
+    def CaballoGanador( self):
+        # DEVUELVE UNA TUPLA CON LOS CaballoGanador DE LA CARRERA
+
+        return tuple( self.__CaballoGanador)
+
+    def CantidadInscritos( self):
+        # DEVUELVE EL NÚMERO DE CABALLOS QUE ESTÁN EN LA CARRERA
+        
+        return len( self.__caballos)
+
+    def MostrarTurno( self):
+        # MUESTRA EL ESTADO DE LA CARRERA HASTA ESE MOMENTO
+
+        for caballo, datos in self.__caballos.items():
+            print( "%2d-%12s: %s" % (datos["dorsal"], caballo.nombre[:12], "="*datos["pasos"]))
+
+        print( "%15s:%s" % ("Meta", " "*self.__PasosMeta + "|"))
             
-        for x in range(1):
-            my_progress['value'] += caballo1
-            my_progress2['value'] += caballo2
-            my_progress3['value'] += caballo3
-            my_progress4['value'] += caballo4
-            root.update_idletasks()
-            time.sleep(0.5)
+    def AñadirCaballo( self, caballo):
+        # REGISTRA LOS CABALLOS A PARTICIPAR EN LA CARRERA
 
+        self.__caballos[caballo] = {"pasos": 0, "puesto": None,
+                                    "dorsal": self.CantidadInscritos()+1}
+        caballo.AñadirCarrera( self)
+
+    def AvanzarTurno( self):
+        # AVANZA UN TURNO EN TODOS LOS CABALLOS QUE ESTÁN EN LA CARRERA
+
+        esFinalDeCarrera = False
+        for caballo, datos in self.__caballos.items():
+            caballo.Avanzar()
+            datos["pasos"] = caballo.PasosCarrera
+            if (not esFinalDeCarrera and
+                caballo.PasosCarrera >= self.PasosMeta):
+                esFinalDeCarrera = True
+        return esFinalDeCarrera
+
+    def Puestos( self):
+        # CALCULA EN QUE PUESTO QUEDÓ CADA CABALLO, ASÍ TAMBIÉN GUARDA EL CABALLO GANADOR
+   
+        self.__clasificacion = sorted(self.__caballos.items(), key=lambda x: x[1]["pasos"], reverse=True)
+        puesto = 1
+        pasosAnterior = self.__clasificacion[0][1]["pasos"]
+        for caballo, datos in self.__clasificacion:
+           if datos["pasos"] < pasosAnterior:
+                puesto += 1
+                pasosAnterior = datos["pasos"]
+           elif puesto == 1: self.__CaballoGanador.append( caballo)
+           datos["puesto"] = puesto
+            
+    def PuestoFinal( self, caballo):
+        # DEVUELVE EL PUESTO EN QUE QUEDÓ CADA CABALLO
+
+        return self.__caballos[caballo]["puesto"]
         
-        print("Caballo 1: " + str(caballo1))
-        print("Caballo 2: " + str(caballo2)) 
-        print("Caballo 3: " + str(caballo3)) 
-        print("Caballo 4: " + str(caballo4)) 
+    def Ganador( self, caballo):
+        # DEVUELVE SI ES QUE UN CABALLO GANÓ LA CARRERA O NO
 
-    if opcion == 'Caballo 1' and caballo1 > 30:
-        pop_label = Label(pop, text="El caballo 1 Ganó")
-        pop_label.pack(pady=10)
-    elif opcion == 'Caballo 1' and caballo1 < 30:
-        pop_label = Label(pop, text="El caballo 1 perdió")
-        pop_label.pack(pady=10)
+        return caballo in self.__CaballoGanador
 
-    if opcion == 'Caballo 2' and caballo2 > 30:
-        pop_label = Label(pop, text="El caballo 2 Ganó")
-        pop_label.pack(pady=10)
-    elif opcion == 'Caballo 2' and caballo2 < 30:    
-        pop_label = Label(pop, text="El caballo 2 perdió")
-        pop_label.pack(pady=10)
+    def TablaDeResultados( self):
+        # MUESTRA EN LA TERMINAL EL PUESTO EN QUE QUEDÓ CADA CABALLO
 
-    if opcion == 'Caballo 3' and caballo3 > 30:
-        pop_label = Label(pop, text="El caballo 3 Ganó")
-        pop_label.pack(pady=10)
-    elif opcion == 'Caballo 3' and caballo3 < 30:    
-        pop_label = Label(pop, text="El caballo 3 perdió")
-        pop_label.pack(pady=10)
-        
-    if opcion == 'Caballo 4' and caballo4 > 30:
-        pop_label = Label(pop, text="El caballo 4 Ganó")
-        pop_label.pack(pady=10)
-    elif opcion == 'Caballo 4' and caballo4 < 30:    
-        pop_label = Label(pop, text="El caballo 4 perdió") 
-        pop_label.pack(pady=10)
+        print( "\n######################## TABLA DE RESULTADOS ########################\n")
+        for caballo, datos in self.__clasificacion:
+            print( "%dº.- Caballo %2d | Nombre: %2s" % 
+                   (datos["puesto"], datos ["dorsal"], caballo.nombre))
+     
+while True:
+    pasos = input( "\nPASOS PARA LLEGAR A LA META: %d \n\nPresione ENTER para comenzar..." % Carrera.PASOS_META())
+    try:
+        carrera = Carrera( pasos)
+    except ArgumentoException as e:
+        print( e)
+        continue
+    break
 
-        
-#label boton
-def click():
-    #AQUÍ SE MUESTRA LA VENTANA DEL FINAL DE LA CARRERA JUNTO AL TEXTO DE QUE SI GANÓ O PERDIÓ
+while True:
+    numeroDeCaballos = input( "\nIntroduce el número de caballos: ")
+    if not numeroDeCaballos.strip(): continue
+    try:
+        numeroDeCaballos = int( numeroDeCaballos)
+    except ValueError as e:
+        print( e)
+        print( "Debes introducir un número de caballos participantes.")
+        continue
+    if numeroDeCaballos > 1: break
+
+    print( "Debes introducir al menos dos caballos.")
+
+for i in range( numeroDeCaballos):
+    while True:
+        print( "\nIntroduce el nombre del Caballo %d" % (i+1))
+        nombre = input( "\tNombre: ")
+        try:
+            caballo = Caballo( nombre)
+        except ArgumentoException as e:
+            print( e)
+            print( "Introduce el nombre del caballo correctamente")
+            continue
+        break
+    carrera.AñadirCaballo( caballo)
+
+print( "\nLa carrera está por comenzar ...\n")
+
+esFinalDeCarrera = False
+while not esFinalDeCarrera: 
+    time.sleep(1)
+    esFinalDeCarrera = carrera.AvanzarTurno()
+    carrera.MostrarTurno()
     
-    global pop
-    pop = Toplevel(root) 
-    pop.iconbitmap('cab-vec.ico')
-    pop.title("Final de la carrera")
-    pop.geometry("350x200")
-    
-    carrera()
-
-    my_frame = Frame(pop)
-    my_frame.pack(pady=5)
-
-OptionList = [
-"Caballo 1",
-"Caballo 2",
-"Caballo 3",
-"Caballo 4"
-] 
-
-###############################################     INTERFAZ   ######################################################################################################
-root = tk.Tk()
-
-bit = root.iconbitmap('cab-vec.ico')
-root.geometry('800x1000')
-root.resizable(False, False)
-root.title('Hipódromo')
-
-# label
-label = ttk.Label(text="A qué caballo desea apostar:")
-label.pack(fill=tk.X, padx=5, pady=5)
-
-variable = tk.StringVar(root)
-variable.set(OptionList[0])
-
-opt = tk.OptionMenu(root, variable, *OptionList)
-opt.config(width=90)
-opt.pack(side="top")
-
-labelTest = tk.Label(text="")
-labelTest.pack(side="top")
-
-variable.trace("w", callback)
-
-# label 2
-label = ttk.Label(text="Cuanto desea apostar al caballo: ")
-label.pack(fill=tk.X, padx=5, pady=5)
-
-#campo de texto
-apuesta=tk.StringVar()
-apuesta=tk.Entry(root,textvariable=apuesta)
-apuesta.pack(fill=tk.X, padx=5, pady=5)
-
-button = tk.Button(root, text='Apostar', width=5, command=click)
-button.pack(fill=tk.X, padx=5, pady=5)
-
-my_label = Label(root, text="")
-my_label.pack(pady=20)
-#######################################################################################################################################################################
-
-###############################################     BARRA DE PROGRESO    ##############################################################################################
-
-label = ttk.Label(text="Caballo 1 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress.pack(pady=20)
-
-label = ttk.Label(text="Caballo 2 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress2 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress2.pack(pady=20)
-
-
-label = ttk.Label(text="Caballo 3 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress3 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress3.pack(pady=20)
-
-
-label = ttk.Label(text="Caballo 4 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress4 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress4.pack(pady=20)
-
-label = ttk.Label(text="Caballo 5 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress5 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress5.pack(pady=20)
-
-label = ttk.Label(text="Caballo 6 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress6 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress6.pack(pady=20)
-
-label = ttk.Label(text="Caballo 7 ")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-my_progress7 = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
-my_progress7.pack(pady=20)
-
-my_button = Button(root, text="Reiniciar", command=stop)
-my_button.pack(pady=20)
-
-my_label = Label(root, text="")
-my_label.pack(pady=20)
-########################################################################################################################################################################
-root.mainloop()
+carrera.Puestos()
+carrera.TablaDeResultados()
+print( "\nCaballo Ganador: \n" + str( [caballo.nombre for caballo in carrera.CaballoGanador]))
